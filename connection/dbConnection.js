69fbc39b988a699956c0ae0441913c10ea817ada
Madhu Korada <korada27@gmail.com>
2018-11-30 00:27:37 +0530

    Update dbConnection.js

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -1,7 +1,7 @@
 const sequelize = require('sequelize');
 const config=require('../config/dbConfig.json');
 const Op = sequelize.Op;
-
+var logger = require('../util/logger');
 //Database Initialization using Sequalize (ORM)
 var connection = new sequelize(config.mariaDB.db, config.mariaDB.username, config.mariaDB.password, {
   host: config.mariaDB.host,
@@ -15,13 +15,15 @@ var connection = new sequelize(config.mariaDB.db, config.mariaDB.username, confi
     idle: 10000
   }
 });
-
+ 
 connection.authenticate()
   .then(function () {
-    console.log("MariaDB Connection Established");
+    // console.log("MariaDB Connection Established");    
+    logger.info("MariaDB Connection Established");
   })
   .catch(function (err) {
-    console.log("Error Connecting to Database" + JSON.stringify(err));
+    // console.log("Error Connecting to Database" +err);
+    logger.error("Error Connecting to Database" + err);
   })
   .done();
 
